ce8e0ab7e4d4793325b9a8dea6d279528c5a3916
Eric Kamau <kamauericm@gmail.com>
2021-07-22 21:44:49 +0300

    updated settings

--- rssfeedreader_web/settings.py
+++ rssfeedreader_web/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DEV_RSSFEEDREADERWEB_SECRET_KEY')
-# SECRET_KEY = os.environ.get('PROD_RSSFEEDREADERWEB_SECRET_KEY')
+# SECRET_KEY = os.environ.get('DEV_RSSFEEDREADERWEB_SECRET_KEY')
+SECRET_KEY = os.environ.get('PROD_RSSFEEDREADERWEB_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
