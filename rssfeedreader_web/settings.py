f76f7ca151c5146fe8904d9f34dfbc00bf403a62
Eric Kamau <kamauericm@gmail.com>
2021-07-24 01:36:32 +0300

    updated settings and staticfiles

--- rssfeedreader_web/settings.py
+++ rssfeedreader_web/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -182,5 +184,3 @@ LOGGING = {
         },
     }
 }
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
