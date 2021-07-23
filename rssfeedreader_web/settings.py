626207cb3f51caf4b82803f84aee2c14159b9b96
Eric Kamau <kamauericm@gmail.com>
2021-07-23 21:52:52 +0300

    updated settings and requirements

--- rssfeedreader_web/settings.py
+++ rssfeedreader_web/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
 import django_heroku
 from pathlib import Path
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('DEV_RSSFEEDREADERWEB_SECRET_KEY')
-SECRET_KEY = os.environ.get('PROD_RSSFEEDREADERWEB_SECRET_KEY')
+# SECRET_KEY = os.environ.get('PROD_RSSFEEDREADERWEB_SECRET_KEY')
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
