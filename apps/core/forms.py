75f3a1626bbdc913f8f9d1ae0a55eccc604b68bf
Eric Kamau <kamauericm@gmail.com>
2021-07-24 04:09:55 +0300

    updated staticfiles

--- apps/core/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class Form(forms.Form):
-    rss_feed_url = forms.URLField(label='Feed URL')
--- apps/core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.core.models import Feed
-
-
-class FeedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feed
-        fields = ('id', 'url')
